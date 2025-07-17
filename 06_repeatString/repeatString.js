c9ede60c94734f96fa871ae27c050cf169e23762
rynerson <lrry223@uky.edu>
2025-07-17 18:51:29 -0400

    complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let repeat_string = "";
+    if(num < 0){
+        repeat_string = repeat_string + "ERROR";
+    }
+    else{
+        for(i = 0; i<num;i++){
+            repeat_string=repeat_string+string;
+     
+        }
+}
+    return repeat_string;
 
 };
 
