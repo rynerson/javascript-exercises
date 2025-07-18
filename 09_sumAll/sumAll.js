e8e94daa19c4591b88faa561fdfbdbb2e6b57258
rynerson <lrry223@uky.edu>
2025-07-18 10:14:20 -0400

    complete exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let total = 0;
+    //Error states
+    if(num1 < 0 || num2 <0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        total = "ERROR";
+    }
+    //goal is to add all numbers from one number to the other
+    else if(num1<=num2){
+    for(let i = num1;i<=num2;i++){
+        total = total+i;
+
+    }
+}
+//goal is to add all numbers between one number and the other
+else if(num1>num2){
+    for(let i = num2; i<=num1;i++){
+        total = total+i;
+    }
+
+}
+    return total;
 
 };
 
