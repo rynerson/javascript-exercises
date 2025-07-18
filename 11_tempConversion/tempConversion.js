7ca4d7d74efffb07c0ed9c43a438d11b75182e7b
rynerson <lrry223@uky.edu>
2025-07-18 10:42:15 -0400

    complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let final_result = (5/9)*(temp-32);
+  return Math.round(final_result*10)/10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let final_result = (9/5)*(temp)+32;
+  return Math.round(final_result*10)/10;
 };
 
 // Do not edit below this line
