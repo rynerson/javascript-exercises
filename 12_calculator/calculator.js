466c0b3670f81de2297cd3285405f875c31f3239
rynerson <lrry223@uky.edu>
2025-07-31 10:23:32 -0400

    complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,49 @@
-const add = function() {
+const add = function(val1,val2) {
+  return val1 + val2;
+  
+
 	
 };
 
-const subtract = function() {
+const subtract = function(val1, val2) {
+  return val1 - val2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let result = array.reduce((sum, current) => sum + current, 0);
+  return result;
+
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+   let result = array.reduce((mult, current) => mult * current, 1);
+  return result;
+  
 
 };
 
-const power = function() {
+const power = function(num1,num2) {
+  return num1 ** num2;
 	
 };
 
-const factorial = function() {
+const factorial = function(num1) {
+  if(num1 < 0){
+    return "Factorial not defined for negative numbers";
+  }
+  if(num1 === 0 || num1 === 1){
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= num1; i++) {
+    result *= i;
+  }
+  return result;
+  
+
+
 	
 };
 
