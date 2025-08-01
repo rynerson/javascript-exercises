2b12f834a25ba01231b6fc85efb48ca5a3283b78
rynerson <lrry223@uky.edu>
2025-08-01 13:16:47 -0400

    complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(my_num) {
+    let a = 0;
+    let b = 1;
+    if(typeof my_num === "string"){
+        my_num = Number(my_num);
+    }
+    if(my_num < 0){
+        return "OOPS";
+    }
+    else{
+        for(let i = 0; i<my_num;i++){
+            let temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a;
+}
 
 };
 
