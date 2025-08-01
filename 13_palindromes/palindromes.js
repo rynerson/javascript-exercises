b1ef1ac2fc9e989c31f66910b9f7dccb033bbd52
rynerson <lrry223@uky.edu>
2025-08-01 12:53:07 -0400

    complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (my_string) {
+  const cleaned = my_string.toLowerCase().replace(/[^a-z0-9]/g,"");
+  const reverse = cleaned.split("").reverse().join("");
+  return cleaned === reverse;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+   
+
+
